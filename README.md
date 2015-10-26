77a62c32881aea8802d3bc384563d25376c65c34
Olivier Poitrey <rs@dailymotion.com>
2015-10-26 16:47:15 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Go HTTP Handler Access Log
+
+[![godoc](http://img.shields.io/badge/godoc-reference-blue.svg?style=flat)](https://godoc.org/github.com/rs/xaccess) [![license](http://img.shields.io/badge/license-MIT-red.svg?style=flat)](https://raw.githubusercontent.com/rs/xaccess/master/LICENSE) [![Build Status](https://travis-ci.org/rs/xaccess.svg)](https://travis-ci.org/rs/xaccess) [![Coverage](http://gocover.io/_badge/github.com/rs/xaccess)](http://gocover.io/github.com/rs/xaccess)
+
+Package xaccess is a middleware that logs all access requests performed on the sub handler using [xlog](https://github.com/rs/xlog) and [xstats](https://github.com/rs/xstats) stored in context if any.
+
+## Licenses
+
+All source code is licensed under the [MIT License](https://raw.github.com/rs/xlog/master/LICENSE).
---
+++ xaccess_test.go
@@ -0,0 +1,81 @@
+package xaccess
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"runtime"
+	"testing"
+	"time"
+
+	"github.com/rs/xhandler"
+	"github.com/rs/xlog"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/context"
+)
+
+type fakeContext struct {
+	err error
+}
+
+func (c fakeContext) Err() error {
+	return c.err
+}
+
+func (c fakeContext) Deadline() (deadline time.Time, ok bool) {
+	return time.Now(), true
+}
+
+func (c fakeContext) Done() <-chan struct{} {
+	return make(chan struct{})
+}
+
+func (c fakeContext) Value(key interface{}) interface{} {
+	return nil
+}
+
+type recorderOutput struct {
+	last map[string]interface{}
+}
+
+func (o *recorderOutput) Write(fields map[string]interface{}) (err error) {
+	o.last = map[string]interface{}{}
+	for k, v := range fields {
+		o.last[k] = v
+	}
+	return nil
+}
+
+func TestResponseStatus(t *testing.T) {
+	assert.Equal(t, "ok", responseStatus(fakeContext{err: nil}, http.StatusOK))
+	assert.Equal(t, "canceled", responseStatus(fakeContext{err: context.Canceled}, http.StatusOK))
+	assert.Equal(t, "timeout", responseStatus(fakeContext{err: context.DeadlineExceeded}, http.StatusOK))
+	assert.Equal(t, "error", responseStatus(fakeContext{err: nil}, http.StatusFound))
+}
+
+func TestServeHTTPC(t *testing.T) {
+	h := NewHandler(xhandler.HandlerFuncC(func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusAccepted)
+		w.Write([]byte{'1', '2', '3'})
+	}))
+	l := xlog.NewHandler(0, h)
+	o := &recorderOutput{}
+	l.SetOutput(o)
+	r, _ := http.NewRequest("GET", "/path", nil)
+	w := httptest.NewRecorder()
+	l.ServeHTTPC(context.Background(), w, r)
+	runtime.Gosched()
+	for i := 0; len(o.last) == 0 && i < 100; i++ {
+		time.Sleep(10 * time.Millisecond)
+	}
+	if assert.NotNil(t, o.last) {
+		assert.Equal(t, 3, o.last["size"])
+		assert.Equal(t, "info", o.last["level"])
+		assert.Equal(t, "access", o.last["type"])
+		assert.Equal(t, "GET", o.last["method"])
+		assert.Equal(t, "ok", o.last["status"])
+		assert.Equal(t, 202, o.last["status_code"])
+		assert.Equal(t, "GET /path 202", o.last["message"])
+		assert.NotEmpty(t, o.last["duration"])
+		assert.NotEmpty(t, o.last["time"])
+	}
+}
